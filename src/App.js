c31ddc5cd73254465aa2288ca5475ae318d8aa0a
Murilo Pita <murilo.junior95@hotmail.com>
2022-12-10 02:22:56 -0300

    Landpage Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// Components
+import { Button } from './components/Button';
+import { Header } from './components/Header';
+import { Input } from './components/Input';
+
+// Styles, icons
+import { MdEmail, MdLock } from 'react-icons/md'
+import { FaUser } from 'react-icons/fa'
+import { 
+   Container, 
+   Title, 
+   Column, 
+   TitleCadastro, 
+   SubtitleCadastro, 
+   EsqueciText, 
+   CriarText,
+   Row,   
+   Wrapper 
+} from './styles';
 
 function App() {
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Header />
+   <Container>
+      <Column>
+         <Title>A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas.
+         </Title>
+      </Column>
+      <Column>
+         <Wrapper>
+            <TitleCadastro>Comece agora grátis</TitleCadastro>
+            <SubtitleCadastro>Crie sua conta e make the change._</SubtitleCadastro>
+            <form>
+               <Input 
+                  placeholder="Nome completo" 
+                  leftIcon={<FaUser />} 
+                  name="Nome"  
+               />
+               <Input 
+                  placeholder="E-mail" 
+                  leftIcon={<MdEmail />} 
+                  name="email"  
+               />
+               <Input 
+                  type="password" 
+                  placeholder="Senha" 
+                  leftIcon={<MdLock />}  
+                  name="senha" 
+               />
+               <Button 
+                  title="Criar minha conta" 
+                  variant="secondary" 
+                  type="submit"
+               />
+            </form>
+            <Column>
+               <EsqueciText>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.</EsqueciText>
+               <Row>
+                  Já tenho conta.<CriarText>Fazer Login</CriarText>
+               </Row>
+            </Column>
+         </Wrapper>
+      </Column>
+   </Container>
+</>
   );
 }
 
